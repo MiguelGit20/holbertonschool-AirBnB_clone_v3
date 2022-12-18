1e1e101923ce56693d61522222ea7def4ee9efda
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-12-18 01:09:03 -0500

    modifying debug

--- api/v1/app.py
+++ api/v1/app.py
@@ -33,4 +33,4 @@ if __name__ == "__main__":
     host_api = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port_api = os.getenv('HBNB_API_PORT', 5000)
 
-    app.run(port=port_api, host=host_api, threaded=True)
+    app.run(port=port_api, host=host_api, threaded=True, debug=True)
