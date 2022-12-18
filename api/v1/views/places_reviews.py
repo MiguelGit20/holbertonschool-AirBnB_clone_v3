08960e315fbbb55c47a04304d83f7352027f6013
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-12-18 15:58:09 -0500

    place review

---
+++ api/v1/views/places_reviews.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""
+Create a new view for Reviw objects
+that handles all default RESTFul API actions.
+"""
+from api.v1.views import app_views
+from models import storage
+from models.place import Place
+from models.review import Review
+from flask import jsonify, make_response, request
+
+@app_views.route('/places/<place_id>/reviews', methods=['GET', 'POST'])
+def review_per_place(place_id=None):
+    """
+        review route to handle http method for requested reviews by place
+    """
+    place_obj = storage.get(Place, place_id)
+    if place_obj is None:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
+    if request.method == 'GET':
+        all_review = storage.all(Review)
+        review_places = [obj.to_json() for obj in all_review.values()
+                       if obj.place_id == place_id]
+        return jsonify(review_places)
+
+    if request.method == 'POST':
+        req_json = request.get_json()
+        if req_json is None:
+            return make_response(jsonify({'error': 'Not found'}), 404)
+        user_id = req_json.get("user_id")
+        if user_id is None:
+            return make_response(jsonify({'error': 'Not found'}), 404)
+        user_obj = storage.get('User', user_id)
+        if user_obj is None:
+            return make_response(jsonify({'error': 'Not found'}), 404)
+        if req_json.get("name") is None:
+            return make_response(jsonify({'error': 'Not found'}), 404)
+
+        req_json['place_id'] = place_id
+        new_object = Review(**req_json)
+        new_object.save()
+        return make_response(jsonify(new_object.to_json()), 201)
+
+@app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
+def retrieve_review_objs_id(review_id):
+    """Retrieves the list of all Place objects with a specified id."""
+    obj = storage.get(Review, review_id)
+    if obj is None:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify(obj.to_dict())
+
+
+@app_views.route('/reviews/<review_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete(review_id):
+    """Deletes an object."""
+    obj = storage.get(Review, review_id)
+    if obj is None:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    storage.delete(obj)
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
+def update(review_id):
+    """Updates an object."""
+    obj = storage.get(Review, review_id)
+    if obj is None:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
+    obj_attr = request.get_json()
+    if obj_attr is None:
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+
+    for key, val in obj_attr.items():
+        if key != ('id', 'updated_at', 'created_at'):
+            setattr(obj, key, val)
+    obj.save()
+    return make_response(jsonify(obj.to_dict()), 200)
