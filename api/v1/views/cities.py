64b1aaa6ed4916fc086e86aeade625dc9a5aec92
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-12-18 15:12:45 -0500

    Some bugs fixed

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,7 +10,8 @@ from models.city import City
 from flask import jsonify, make_response, request
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities',
+                 methods=['GET'], strict_slashes=False)
 def retrieve_city_objs_route(state_id):
     """Retrieves the list of all City objects by a state."""
     obj_id = storage.get(State, state_id)
@@ -19,21 +20,21 @@ def retrieve_city_objs_route(state_id):
         cities_list = []
         for o in obj.values():
             if o.state_id == state_id:
-                cities_list.append(o)
+                cities_list.append(o.to_dict())
         return jsonify(cities_list)
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@app_views.route('/citites/<city_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def retrieve_city_obj_route(city_id):
     """Retrieves a city with a specified id."""
     obj_id = storage.get(City, city_id)
     if obj_id is not None:
-        return jsonify(obj_id)
+        return jsonify(obj_id.to_dict())
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@app_views.route('/citites/<city_id>', methods=['DELETE'],
+@app_views.route('/cities/<city_id>', methods=['DELETE'],
                  strict_slashes=False)
 def deletes_city_route(city_id):
     """Deletes an object."""
@@ -45,7 +46,8 @@ def deletes_city_route(city_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities',
+                 methods=['POST'], strict_slashes=False)
 def creates_instance_city_route(state_id):
     """Creates a new object."""
     obj_attr = request.get_json()
@@ -56,13 +58,13 @@ def creates_instance_city_route(state_id):
     obj_id = storage.get(State, state_id)
     if obj_id is None:
         return make_response(jsonify({'error': 'Not found'}), 404)
-    obj_attr[state_id] = state_id
     new_instance = City(**obj_attr)
+    new_instance.state_id = state_id
     new_instance.save()
     return make_response(jsonify(new_instance.to_dict()), 201)
 
 
-@app_views.route('/citites/<city_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def updates_city_route(city_id):
     """Updates an object."""
     obj = storage.get(City, city_id)
